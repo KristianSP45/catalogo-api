54aeee5e2df32cc4c55224e6a4930fc9a320b3d7
Kristian Pessoa <sousakristian45@gmail.com>
2026-07-29 14:50:45 -0300

    first commit

---
+++ src/main/java/com/kristian/catalogoapi/CatalogoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kristian.catalogoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoApiApplication.class, args);
+	}
+
+}
